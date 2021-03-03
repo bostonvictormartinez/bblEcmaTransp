ae159066e03e7dd1714dc7f72294aad92c061117
bostonvictormartinez <57546685+bostonvictormartinez@users.noreply.github.com>
2021-03-03 17:41:36 -0500

    ecma2example

---
+++ index.mjs
@@ -0,0 +1,19 @@
+let helloWorld='hello world';
+console.log(`${helloWorld} is response`)
+
+import{square, diag} from './lib.js';
+//add file lib.js
+
+ //this does not work
+
+ //npm install -D babel-cli
+
+ //npm install -D babel-preset-es2015
+
+ //add .babelrc  {"presets":['es1015']}
+
+//new index.mjs repeat index.js 
+
+var sqrt=square(2)
+console.log(sqrt)  //returnns cant import and unexp token
+
